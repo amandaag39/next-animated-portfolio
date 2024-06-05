3749cc128cd02276c349a516cf46e3c55edd2bd4
amandaag39 <amandaag39@gmail.com>
2024-06-05 10:22:28 -0500

    added about page changes, animation for page changes

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,17 @@
+"use client"
+import {motion} from "framer-motion"
+
 const PortfolioPage = () => {
     return (
-        <div className='portfolio-page'>PortfolioPage</div>
-    )
-}
+        <motion.div 
+            className="h-full" 
+            initial={{ y: "-200vh" }} 
+            animate={{ y: "0%" }} 
+            transition={{ duration: 1 }}
+            >
+                Portfolio
+            </motion.div>
+    );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
