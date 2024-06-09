67c8fc4eeaa992c159e802da185c37b0fb75f846
amandaag39 <amandaag39@gmail.com>
2024-06-08 22:36:30 -0500

    set up email through contact form changed favicon

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,37 @@
 "use client"
-import {motion} from "framer-motion"
-import { useState } from "react";
+import {motion} from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactPage = () => {
     const [success, setSuccess] = useState(false)
     const [error, setError] = useState(false)
     const text = "Reach out "
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setError(false);
+        setSuccess(false);
+    
+        emailjs
+          .sendForm(
+            process.env.NEXT_PUBLIC_SERVICE_ID,
+            process.env.NEXT_PUBLIC_TEMPLATE_ID,
+            form.current,
+            process.env.NEXT_PUBLIC_PUBLIC_KEY
+          )
+          .then(
+            () => {
+              setSuccess(true);
+              form.current.reset();
+            },
+            () => {
+              setError(true);
+            }
+          );
+      };
 
     return (
         <motion.div 
@@ -27,11 +53,11 @@ const ContactPage = () => {
                         </div>
                     </div>
                     {/* FORM CONTAINER */}
-                    <form className="h-1/2 lg:h-full lg:w-1/2 bg-neutral-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
+                    <form onSubmit={sendEmail} ref={form} className="h-1/2 lg:h-full lg:w-1/2 bg-neutral-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
                         <span>Dear Amanda Mendez,</span>
-                        <textarea rows={6} className="bg-transparent border-2 outline-none resize-none" />
+                        <textarea rows={6} className="bg-transparent border-2 outline-none resize-none" name="user_message"/>
                         <span>My email address is:</span>
-                        <input type="text" className="bg-transparent border-2 outline-none" />
+                        <input type="text" className="bg-transparent border-2 outline-none" name="user_email"/>
                         <span>Regards</span>
                         <button className="bg-teal-500 rounded font-semibold text-white p-4">Send</button>
                         {success && <span className="text-green-600 font-semibold">Your message has been sent successfully!</span>}
